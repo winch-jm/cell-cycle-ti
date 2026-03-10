99c3d42b175832ad3ed55f66ae92f92643de0cef
anoushkasamuel <anoushkasamuel@gmail.com>
2026-03-09 21:19:21 -0400

    preprocessing of data

---
+++ data/preprocess/preprocess.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import anndata as ad
+import scanpy as sc
+
+# Load raw data
+wt_data = pd.read_csv(
+    '/Users/anoushkasamuel/go/src/cell-cycle-ti/data/GSE142277/GSM4224315_out_gene_exon_tagged.dge_exonssf002_WT.txt',
+    sep='\t',
+    index_col=0
+)
+
+# genes × cells → transpose to cells × genes for AnnData
+adata = ad.AnnData(X=wt_data.T.astype(float))
+
+# log1p
+sc.pp.log1p(adata)
+
+# Highly variable genes
+sc.pp.highly_variable_genes(adata, n_top_genes=2000)
+adata = adata[:, adata.var.highly_variable].copy()
+
+# PCA 
+sc.tl.pca(adata, n_comps=50)
+
+print(adata)
+print("PCA shape:", adata.obsm['X_pca'].shape)
